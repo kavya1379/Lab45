aba2f95c90555c63c6cb31bef5692340526cf9a2
kavya1379 <horkerikavya@gmail.com>
2025-11-24 16:39:20 +0530

    Fix formatting of print statement in addition2.py

--- addition2.py
+++ addition2.py
@@ -6,4 +6,4 @@ def add(a, b):
 if _name_ == "_main_":
     x = int(sys.argv[1])   # Convert first argument to integer
     y = int(sys.argv[2])   # Convert second argument to integer
-    print("sum:", add(x, y))
+    print("sum:",add(x, y))
