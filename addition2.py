708b42b927eec219fd9c328d03dc55559b6ccd40
kavya1379 <horkerikavya@gmail.com>
2025-11-24 16:29:03 +0530

    Create addition2.py

---
+++ addition2.py
@@ -0,0 +1,11 @@
+import sys
+if (sys.argv) == 2:
+  x=int(sys.argv[1])
+  y=int(sys.argv[2])
+else:  
+def add(a, b):
+    return a+b
+if __name__ == "__main__":
+    x=10
+    y=20
+    print("Sum:",add(x, y))
